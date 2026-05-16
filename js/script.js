92ca4d10d6a96abf5ac6e89874028e42a63347a2
Sakira1245 <sakiramahmud50@gmail.com>
2026-05-16 18:00:23 +0600

    load issue details and display issue details completed

--- js/script.js
+++ js/script.js
@@ -22,6 +22,52 @@ const loadIssues = () => {
 }
 loadIssues();
 
+let allIssues = [];
+
+const loadIssueDetail = async (id) => {
+    const url = `https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayIssueDetails(details.data);
+}
+
+const displayIssueDetails = (issue) => {
+    const detailsBox = document.getElementById('issue-details');
+    detailsBox.innerHTML = `<div class="modal-box">
+    <div class="details-container">
+        <div class="">
+            <h2 class="primary-color text-2xl font-bold">${issue.title}</h2>
+            <div class="subsection flex gap-3 mt-2 items-center">
+                <button class="btn text-white font-normal rounded-full h-6 text-xs !border-none ${issue.status === "open" ? "open-status" : "close"}">${issue.status}</button>
+                <p class="secondary-color text-xs">• Opened by ${issue.assignee ? issue.assignee : "No Assignee"}</p>
+                <p class="secondary-color  text-xs">• ${new Date(issue.createdAt).toLocaleDateString()}</p>
+            </div>
+            <div class="button flex gap-2 mt-6">
+                 ${createElements(issue.labels)}
+            </div>
+            <p class="secondary-color font-normal text-base mt-6">${issue.description}</p>
+            <div class="background p-4 grid grid-cols-2 mt-6 rounded-lg">
+                <div class="assignee">
+                    <p class="secondary-color font-normal">Assignee</p>
+                    <p class="primary-color font-semibold">${issue.assignee ? issue.assignee : "No Assignee"}</p>
+                </div>
+                <div class="priority">
+                    <p class="secondary-color font-normal">Priority:</p>
+                    <span class="bug rounded-full py-[6px] font-medium text-xs px-4 !border-none priority-${issue.priority}">${issue.priority.toUpperCase()}</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="modal-action">
+      <form method="dialog">
+        <!-- if there is a button in form, it will close the modal -->
+        <button class="btn btn-primary">Close</button>
+      </form>
+    </div>
+  </div>`;
+    document.getElementById('issue-details').showModal();
+}
+
 const displayIssues = (issues) => {
     manageSpinner(false);
     const container = document.getElementById('case-container');
